0186590ff00b6e20ae7045c4aee01422f4612479
LanceRabbit <lancerabbit@gmail.com>
2020-04-12 12:52:32 +0800

    show thee name of all group on DashBoard

--- src/app/components/Dashboard.jsx
+++ src/app/components/Dashboard.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-export const DashBoard = ({group}) => (
+export const DashBoard = ({groups}) => (
   <div>
     <h2> DashBoard </h2>
+    {groups.map(group => (
+      <div>
+        {group.name}
+      </div>
+    ))}
   </div>
 )
+
+function mapStateToProps(state) {
+  return {
+    groups: state.groups
+  }
+}
+
+export const ConnectedDashBoard = connect(mapStateToProps)(DashBoard);
--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux'
 import { store } from '../store'
+import { ConnectedDashBoard } from './Dashboard'
 
 export const Main = () => (
   <Provider store = {store}>
     <div>
-      Dash bord gogo here
+      {/* Dash bord gogo here */}
+      <ConnectedDashBoard/>
     </div>
   </Provider>
 )
